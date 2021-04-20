0990055089afba9c60f2f2b5a231459e99b91234
DanielCarrami <danielcarrami@gmail.com>
2021-04-20 12:44:00 -0500

    side menu icons

--- hack-frontend/src/app/app.module.ts
+++ hack-frontend/src/app/app.module.ts
@@ -8,7 +8,11 @@ import { RegistrationComponent } from './registration/registration.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {HttpClientModule} from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +24,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     MatSidenavModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hack-frontend/src/app/registration/registration.component.ts
+++ hack-frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-registration',
@@ -8,7 +10,49 @@ import { Component, OnInit } from '@angular/core';
 export class RegistrationComponent implements OnInit {
   step: any = 1;
 
-  constructor() { }
+  ranks = [
+    {name: "Concientizacion", icon: "assets/images/consiencia.png"},
+    {name: "Manejo de residuos", icon: "assets/images/mask.png"}
+  ]
+
+  teamTypes = [
+    {name: "Local", icon: "assets/images/local.png"},
+    {name: "Externo", icon: "assets/images/externo.png"},
+    {name: "Mixto", icon: "assets/images/mixto.png"}
+  ]
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+    ) { 
+      this.matIconRegistry.addSvgIcon(
+        `text`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/text icon.svg`)
+      );
+      this.matIconRegistry.addSvgIcon(
+        `type`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/home icon.svg`)
+      );
+      this.matIconRegistry.addSvgIcon(
+        `rank`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/world icon.svg`)
+      );
+      this.matIconRegistry.addSvgIcon(
+        `info`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/code icon.svg`)
+      );
+      this.matIconRegistry.addSvgIcon(
+        `master`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/hacker master icon.svg`)
+      );
+      this.matIconRegistry.addSvgIcon(
+        `hacker`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/hacker icon.svg`)
+      );
+      /*this.matIconRegistry.addSvgIcon(
+        `almost`,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`assets/icons/box icon.svg`)
+      );*/
+    }
 
   ngOnInit(): void {
   }
