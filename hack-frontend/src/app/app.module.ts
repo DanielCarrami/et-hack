648ec65eaa1762d576d5dd39faa8acc444aaabe9
Oscar Ramirez <A01660338@itesm.mx>
2021-04-22 20:39:49 -0500

    Topics finished

--- hack-frontend/src/app/app.module.ts
+++ hack-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TopicsComponent } from './topics/topics.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    TopicsComponent
   ],
   imports: [
     BrowserModule,
