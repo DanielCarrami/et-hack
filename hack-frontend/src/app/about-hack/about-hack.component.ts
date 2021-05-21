3cce4caf7825867000f10acb63de25d250021b1e
DanielCarrami <danielcarrami@gmail.com>
2021-05-20 23:17:28 -0500

    Expansion About

--- hack-frontend/src/app/about-hack/about-hack.component.ts
+++ hack-frontend/src/app/about-hack/about-hack.component.ts
@@ -6,10 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-hack.component.scss']
 })
 export class AboutHackComponent implements OnInit {
+  backheight: any = '100vh';
+  margintop: any = '0';
+  open: boolean = false;
+  showbutton: any = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  panelOpenState: boolean = false;
+
+  togglePanel() {
+    console.log('Hola');
+    this.panelOpenState = !this.panelOpenState
+  }
+
+  onExpansion(){
+    if(this.open){
+      this.open = false;
+      this.backheight = '100vh';
+      this.margintop = '0';
+      this.showbutton = '';
+    }else{
+      this.open = true;
+      this.backheight = '150vh';
+      this.margintop = '-600';
+      this.showbutton = 'hidden';
+    }
+  }
+
+  setMyStyle(margin: any){
+    let styles = {
+      'margin-top': margin + 'px',
+    };
+    return styles;
   }
 
 }
