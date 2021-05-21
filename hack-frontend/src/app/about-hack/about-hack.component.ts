ae7bc4fa6873d6a897cf2c65172df32b56fa6632
DanielCarrami <danielcarrami@gmail.com>
2021-05-21 10:27:52 -0500

    Responsive About expansion

--- hack-frontend/src/app/about-hack/about-hack.component.ts
+++ hack-frontend/src/app/about-hack/about-hack.component.ts
@@ -10,6 +10,8 @@ export class AboutHackComponent implements OnInit {
   margintop: any = '0';
   open: boolean = false;
   showbutton: any = '';
+  public screenWidth: any;
+  public screenHeight: any;
 
   constructor() { }
 
@@ -19,20 +21,40 @@ export class AboutHackComponent implements OnInit {
   panelOpenState: boolean = false;
 
   togglePanel() {
-    console.log('Hola');
-    this.panelOpenState = !this.panelOpenState
+    this.panelOpenState = !this.panelOpenState;
+    this.backheight = '100vh';
+    this.margintop = '0';
+    this.showbutton = '';
+    this.open = false;
   }
 
   onExpansion(){
+    this.screenWidth = window.innerWidth;
+
+    this.screenHeight = window.innerHeight;
+    console.log(this.screenHeight);
+    console.log(this.screenWidth);
+    console.log('Hello');
     if(this.open){
+      console.log('Open');
       this.open = false;
       this.backheight = '100vh';
       this.margintop = '0';
       this.showbutton = '';
     }else{
+      console.log('Close');
+      if(this.screenWidth < 700){
+        this.backheight = '250vh';
+        this.margintop = '-1200';
+      }else{
+        this.backheight = '150vh';
+        this.margintop = '-500';
+      }
+      if(this.screenHeight < 700 && this.screenWidth > 700){
+        this.backheight = '180vh';
+        this.margintop = '-450';
+      }
       this.open = true;
-      this.backheight = '150vh';
-      this.margintop = '-600';
       this.showbutton = 'hidden';
     }
   }
